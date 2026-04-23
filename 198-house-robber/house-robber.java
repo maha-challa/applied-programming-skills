0119c858777873b57fa92b85d6a233f0c1e5c2a0
maha-challa <mahachalla190@gmail.com>
2026-04-23 14:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (58.23%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n==1){
+            return nums[0];
+        }else if(n==2){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        for(int i=2; i<n; i++){
+            nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
+        }
+
+        return nums[n-1];
+    }
+}
